24de744a4137d55f97e5987be723e96276b61a6b
amanmeena14 <amanujs2003@gmail.com>
2025-05-24 17:09:16 +0530

    cpmleted data fetching part from backend

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
 import Hero from "@/components/Hero"
 import InfoBoxes from "@/components/InfoBoxes"
 import HomeProperties from "@/components/HomeProperties"
+import connectDB from "@/config/database"
 
-const page = () => {
+const page = async() => {
+  await connectDB();
+  console.log(process.env.MONGODB_URI)
   return (
     <>
       <Hero/>
