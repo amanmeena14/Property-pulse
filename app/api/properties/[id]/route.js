816288769da64715a8028565104dea5163f8b576
amanmeena14 <amanujs2003@gmail.com>
2025-06-11 23:45:01 +0530

    completed update functionality for changes in property listings

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
 
 export const GET = async (request,{params})=>{
     try{
@@ -7,6 +8,39 @@ export const GET = async (request,{params})=>{
         const property = await Property.findById(params.id);
         if(!property) return new Response('property not found',{status: 400});
         return new Response(JSON.stringify(property),
+    {
+        status:200,
+    });
+    }
+    catch(error){
+        console.log(error);
+        return new Response('Something went wrong',{
+            status:500
+        });
+    }
+}
+
+export const DELETE = async (request,{params})=>{
+
+    try{
+        const propertyId=params.id;
+        const sessionUser =await getSessionUser();
+
+        if(!session || !session.userId){
+            return new Response('User Id is required',{status:401});
+        }
+
+        const {userId}=sessionUser;
+        await connectDB();
+        const property = await Property.findById(propertyId);
+        if(!property) return new Response('property not found',{status: 404});
+
+        if(property.owner.toString()!==userId){
+            return new Response('Unauthorized',{status:401});
+        }
+
+        await property.deleteone();
+        return new Response('property deleted',
     {
         status:200,
     });
--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,8 @@ import '@/assets/styles/globals.css'
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
+ import { ToastContainer } from 'react-toastify';
+ import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata={
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -46,6 +48,7 @@ const Layout = ({ children }) => {
           <Navbar/>
           <main>{children}</main>
           <Footer/>
+          <ToastContainer/>
         </body>
       </html>
       </AuthProvider>
