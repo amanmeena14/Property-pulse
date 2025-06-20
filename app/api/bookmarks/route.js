c93838bf052edce29b9d7c02e9fcbcfca3678801
amanmeena14 <amanujs2003@gmail.com>
2025-06-20 23:40:32 +0530

    Added search functionality for property searching

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -5,6 +5,28 @@ import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic='force-dynamic';
 
+//get/api/bookmarks
+export const GET =async()=>{
+    try {
+        await connectDB();
+        const sessionUser=await getSessionUser();
+
+        if(!sessionUser || !sessionUser.userId){
+            return new Response('User Id is required',{status:401});
+        }
+
+        const {userId}=sessionUser;
+        const user=await User.findOne({_id:userId});
+        
+        //get user bookmarks
+        const bookmarks=await Property.find({_id: {$in: user.bookmarks}});
+
+        return new Response(JSON.stringify(bookmarks),{status:200});
+    } catch (error) {
+        console.log(error);
+        return new Response('Something went wrong',{status:500});
+    }
+}
 export const POST=async(request)=>{
     try {
         await connectDB();
--- components/PropertyEditForm.jsx
+++ components/PropertyEditForm.jsx
@@ -561,7 +561,7 @@ const PropertyEditForm = () => {
                 className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Add Property
+                Edit Property
               </button>
             </div>
           </form>)
